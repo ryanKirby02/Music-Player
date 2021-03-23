128f37188c5a85ef8d9c26b309b8e62398d83907
RyanCantCode <kirbyryan36@gmail.com>
2021-03-23 09:52:31 -0400

    added the skip track functionality

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './styles/app.scss';
 import Player from './components/Player';
 import Song from './components/Song';
 import Library from './components/Library';
+import Nav from './components/Nav';
 
 import data from './data';
 
@@ -12,18 +13,23 @@ function App() {
   const [songs, setSongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[0]);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [isLibraryOpen, setIsLibraryOpen] = useState(false)
 
   //audio reference
   const audioRef = useRef(null);
 
   return (
     <div className='App'>
+      <Nav isLibraryOpen={isLibraryOpen} setIsLibraryOpen={setIsLibraryOpen} />
       <Song currentSong={currentSong} />
       <Player
+        songs={songs}
         isPlaying={isPlaying}
         setIsPlaying={setIsPlaying}
         currentSong={currentSong}
+        setCurrentSong={setCurrentSong}
         audioRef={audioRef}
+        setSongs={setSongs}
       />
       <Library
         songs={songs}
@@ -31,6 +37,7 @@ function App() {
         audioRef={audioRef}
         isPlaying={isPlaying}
         setSongs={setSongs}
+        isLibraryOpen={isLibraryOpen}
       />
     </div>
   );
